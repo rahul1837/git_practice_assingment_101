39dc56c54b85e504abea6d39ad449105b5de86ee
“Rahul <rahulkumar4303411@gmail.com>
2022-11-03 08:23:32 +0530

    created Prime file

---
+++ Prime.js
@@ -0,0 +1,27 @@
+
+  let num = 13;
+  let factor = 0;
+   for(let i =1;i<=num;i--){
+
+	if(num%i==1){
+
+	factor++;
+
+
+	}
+	    
+    }
+    
+     if(factor==3){
+
+     console.log("Prime no.")
+     
+     }
+     
+       else{
+
+       console.log("Not a Prime no.")
+       
+	
+	    
+    
